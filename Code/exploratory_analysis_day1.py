87fde697efb8f853b4500abe1c3c5db79bea44cc
ktt920 <kaitseng920@gmail.com>
2026-02-22 19:41:02 -0500

    Created a Scatterplot for first data release of illness
    The same code for the scatter plot is included in the exploratory analysis and notebook report.

--- Code/exploratory_analysis_day1.py
+++ Code/exploratory_analysis_day1.py
@@ -1,10 +1,25 @@
 #%%
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 #%%
 # Load the data
 data = pd.read_csv('../Data/mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
 
 #%%
-# Make a plot of the active cases over time
+#Extract the information from data. The x-variable will be the day progressed with the y variable being the number of active cases
+
+x = data['day']
+y = data['active reported daily cases']
+
+
+# Plotting the scatter graph
+plt.scatter(x, y, alpha=0.6, color='green')
+plt.title('Progression of Mystery Illness by Dates')
+plt.xlabel('Days')
+plt.ylabel('Active Reported Daily Cases')
+
+plt.show()
+# %%
